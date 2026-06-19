9ad0928e394cba6de7d861291ab2168646aaaee8
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2026-06-19 22:31:23 +0200

    Initial commit

---
+++ src/env.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+const EnvSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "test", "production"])
+    .default("development"),
+  CLIENT_ID: z.string().min(1),
+  DISCORD_TOKEN: z.string().min(1),
+  DEV_GUILD_ID: z.string().min(1),
+});
+
+export const env = EnvSchema.parse(process.env);
